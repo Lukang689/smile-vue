c8cfb1bccf0d3b6e833124223790dcfa355a3338
lukang <lukang8596@outlook.com>
2018-09-19 11:11:08 +0800

    导入有赞vant各个组件

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import {Button, Row, Col, Icon, Field, Swipe, SwipeItem, Lazyload} from 'vant'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Button).use(Row).use(Col).use(Icon).use(Field).use(Swipe).use(SwipeItem).use(Lazyload)
 
 /* eslint-disable no-new */
 new Vue({
